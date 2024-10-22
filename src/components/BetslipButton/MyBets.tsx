6b2beb8c21bb4064a892cbe10d4299a512203d4a
Nabeel Abrahams <88242933+NabeelAbrahams@users.noreply.github.com>
2024-10-23 00:49:38 +0200

    fix mybets typing

--- src/components/BetslipButton/MyBets.tsx
+++ src/components/BetslipButton/MyBets.tsx
@@ -1,49 +1,48 @@
 'use client'
 import { MyBetFilterNotFound, MyBetNotFound } from '@/components/NotFound'
 import { returnTypeOfBet } from '@/utils'
+import type { ApolloError } from '@apollo/client'
 import { Bet, BetType } from '@azuro-org/sdk'
 import { useMemo, useState } from 'react'
 import { SkeletonArray } from '../Skeleton'
 import Tabs from '../Tabs'
 import WrapBetCard from './WrapBetCard'
-import type { ApolloError } from '@apollo/client'
 
 type MyBetsProps = {
   tabs: {
-    name: string;
-    type: BetType;
-    count: number;
-  }[],
+    name: string
+    type: BetType
+    count: number
+  }[]
   prematchBets: {
-    loading: boolean;
-    bets: Bet[];
-    error: ApolloError | undefined;
+    loading: boolean
+    bets: Bet[]
+    error: ApolloError | undefined
   }
-
 }
 
 export default function MyBets({ tabs, prematchBets }: Readonly<MyBetsProps>) {
   const [value, setValue] = useState(0)
 
   const filterBets = useMemo(() => {
-    const _bets = prematchBets.bets
-    const _data = _bets.filter(
+    const _bets = prematchBets?.bets
+    const _data = _bets?.filter(
       (bet) => returnTypeOfBet(bet) === tabs[value].type
     )
     return {
-      bets: _data.toSorted((prev, curr) => curr.createdAt - prev.createdAt),
+      bets: _data?.toSorted((prev, curr) => curr.createdAt - prev.createdAt),
     }
-  }, [prematchBets.bets, tabs, value])
+  }, [prematchBets?.bets, tabs, value])
 
   const showNoData = useMemo(
-    () => !prematchBets.loading && !prematchBets.bets.length,
-    [prematchBets.bets.length, prematchBets.loading]
+    () => !prematchBets?.loading && !prematchBets?.bets?.length,
+    [prematchBets?.bets.length, prematchBets?.loading]
   )
 
   return (
     <>
       <div>
-        {Boolean(prematchBets.bets.length) && (
+        {Boolean(prematchBets?.bets?.length) && (
           <Tabs items={tabs} value={value} onClick={setValue} />
         )}
       </div>
@@ -51,14 +50,14 @@ export default function MyBets({ tabs, prematchBets }: Readonly<MyBetsProps>) {
         {filterBets?.bets?.map((bet) => (
           <WrapBetCard key={bet.txHash} bet={bet} status={tabs[value].type} />
         ))}
-        {prematchBets.loading && (
+        {prematchBets?.loading && (
           <SkeletonArray length={3} className="mb-5 w-full h-[150px]" />
         )}
         {showNoData ? (
           <MyBetNotFound />
         ) : (
           <>
-            {!prematchBets.loading && filterBets.bets.length === 0 && (
+            {!prematchBets?.loading && filterBets.bets.length === 0 && (
               <MyBetFilterNotFound />
             )}
           </>
